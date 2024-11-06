5c0e9eb5dbaa58b357d57650a785c47cab368d46
ethpch <ethpch@outlook.com>
2024-11-06 15:58:26 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using ReactiveUI;
+using ReactiveUI.SourceGenerators;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            TestClass<int> test = new();
+            test.TestFunction1();
+        }
+    }
+    internal partial class TestClass<T> : ReactiveObject
+    {
+        [Reactive] T? _value;
+        [ReactiveCommand]
+        void TestFunction()
+        {
+            
+        }
+        public void TestFunction1()
+        {
+            Console.WriteLine("Call TestFunction1");
+        }
+    }
+}
